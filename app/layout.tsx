9819895a1c77804e4baa27a23d7da340404bcfe9
NumanTalukder <51156089+NumanTalukder@users.noreply.github.com>
2023-07-11 13:23:26 +0600

    adding grafbase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export const metadata = {
   title: 'Flexibble',
@@ -13,7 +15,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body>
+        <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   )
